94c87254e8d17ae13a59a75926af0a72e047a171
Jhan Carlos Ortegon <75187920+jhanOrtegon@users.noreply.github.com>
2025-12-19 14:12:14 -0500

    Change icon file name from 'icon.svg' to 'icon_ttn.svg'

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/icon_ttn.svg",
         type: "image/svg+xml",
       },
     ],
